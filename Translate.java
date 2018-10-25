f6d66b00008a48d7ad86999d4f0e5b3f29459e15
adam <ibrahimadam193@gmail.com>
2018-10-25 02:24:24 -0400

    Add support for bold, italic, and lists
    Added the ability to specify unordered lists, ordered lists, bold and
    italic.
    
    All elements (with the exception of bold and itaic) added can be used
    anywhere as stated in the specification.  So I've got bold and italic in
    paragraphs, and list items.
    
    The only exception at the moment is that bold/italic cannot be nested in
    one another.
    
    Changed sample to test out new capabilities.
    
    Changes to be committed:
          modified:   Translate.java
          modified:   sample.xml

--- Translate.java
+++ Translate.java
@@ -12,12 +12,19 @@ import org.odftoolkit.odfdom.incubator.doc.text.OdfTextSpan;
 import org.odftoolkit.odfdom.dom.element.text.TextSpanElement;
 
 import org.odftoolkit.simple.text.Paragraph;
+import org.odftoolkit.odfdom.incubator.doc.text.OdfTextParagraph;
+import org.odftoolkit.odfdom.dom.element.text.TextLineBreakElement;
 
 import org.odftoolkit.simple.text.list.List;
 import org.odftoolkit.simple.text.list.ListItem;
+import org.odftoolkit.simple.text.list.BulletDecorator;
+import org.odftoolkit.simple.text.list.NumberDecorator;
 
 import org.odftoolkit.odfdom.pkg.OdfElement;
 import org.odftoolkit.odfdom.pkg.OdfFileDom;
+
+/* For basic font style manipulation (bold, italic) */
+import org.odftoolkit.simple.style.StyleTypeDefinitions.FontStyle;
 
 /* for XML parsing. */
 
@@ -37,6 +44,8 @@ class Translate {
     throws Exception
     {
         doc = TextDocument.newTextDocument();
+        /* Remove initial paragraph. */
+        removeChildren(doc.getContentRoot());
         dom = doc.getContentDom();
     }
 
@@ -64,6 +73,9 @@ class Translate {
             if (name == "p") {
                 Paragraph p = doc.addParagraph("");
                 makeParagraph(child, p);
+            } else if (name == "list" || name == "ul" || name == "ol") {
+                List l = doc.addList();
+                makeList(child, l);
             }
         });
     }
@@ -81,25 +93,108 @@ class Translate {
      * ODFDOM is easier for this.
      */
     public void makeParagraph(Node pNode, Paragraph p) {
-        NodeList children = pNode.getChildNodes();
-
-        for (int i = 0; i < children.getLength(); i++) {
-            Node n = children.item(i);
-            switch (n.getNodeType()) {
+        traverseChildren(pNode, child -> {
+            switch (child.getNodeType()) {
                 case Node.TEXT_NODE :
-                    p.appendTextContent(n.getNodeValue());
+                    String content = child.getNodeValue();
+                    //p.appendTextContent(content);
+                    /* This method of adding text I got from
+                     * org.odftoolkit.simple.text.Paragraph.appendTextElements
+                     * Unfortunately, if there's special whitespace
+                     * characters in the content, they won't be
+                     * handled appropriately. Frankly, I think I'd
+                     * rather handle that at the pollen level than at
+                     * this level, and add a few more tags to the
+                     * specification. This part should really be as
+                     * simple as possible, and just transform an SEXP
+                     * into an ODT document.
+                     */
+                    Text textNode = dom.createTextNode(content);
+                    p.getOdfElement().appendChild(textNode);
                     break;
                 case Node.ELEMENT_NODE :
                     OdfTextSpan s = new OdfTextSpan(dom);
-                    makeSpan(n, Span.getInstanceof(s));
+                    makeSpan(child, Span.getInstanceof(s));
                     p.getOdfElement().appendChild(s);
                     break;
             }
-        }
+        });
     }
 
     public void makeSpan(Node spanNode, Span s) {
         s.appendTextContent(spanNode.getFirstChild().getNodeValue());
+        if (spanNode.getNodeName() == "bold") {
+            s.getStyleHandler().getTextPropertiesForWrite()
+                .setFontStyle(FontStyle.BOLD);
+        } else if (spanNode.getNodeName() == "italic") {
+            s.getStyleHandler().getTextPropertiesForWrite()
+                .setFontStyle(FontStyle.ITALIC);
+        }
+    }
+
+    public void makeList(Node listNode, List list) {
+        removeChildren(list.getOdfElement());
+        String name = listNode.getNodeName();
+        if (name == "ul") {
+            list.setDecorator(new BulletDecorator(doc));
+        } else if (name == "ol") {
+            list.setDecorator(new NumberDecorator(doc));
+        }
+        traverseChildren(listNode, child -> {
+            ListItem li = list.addItem("");
+            makeListItem(child, li);
+        });
+    }
+
+    public void makeUnorderedList(Node listNode, List list) {
+        removeChildren(list.getOdfElement());
+        traverseChildren(listNode, child -> {
+            ListItem li = list.addItem("");
+            makeListItem(child, li);
+        });
+    }
+
+    public void makeListItem(Node liNode, ListItem li) {
+        OdfElement e = li.getOdfElement();
+        removeChildren(e);
+        traverseChildren(liNode, child -> {
+            String name = child.getNodeName();
+            if (name == "p") {
+                OdfTextParagraph previous = 
+                    OdfElement.findFirstChildNode(
+                            OdfTextParagraph.class, e);
+                OdfTextParagraph odfp =
+                    dom.newOdfElement(OdfTextParagraph.class);
+                makeParagraph(child, Paragraph.getInstanceof(odfp));
+                /* It seems that adding paragraphs to a list item
+                 * results in new list items. So in order for the list
+                 * to display correctly, I need to make sure that only
+                 * one paragraph element is actually in the list item.
+                 * However, I don't really care about that from the
+                 * perspective of the SEXP. It should just visually
+                 * look like a new paragraph started.
+                 */
+                if (previous == null) {
+                    e.appendChild(odfp);
+                } else {
+                    TextLineBreakElement lineBreak =
+                        dom.newOdfElement(TextLineBreakElement.class);
+                    previous.appendChild(lineBreak);
+                    traverseChildren(odfp, childOfp -> {
+                        previous.appendChild(childOfp);
+                    });
+                }
+            } else if (name == "list" || name == "ul" || name == "ol") {
+                List subList = li.addList();
+                makeList(child, subList);
+            }
+        });
+    }
+
+    public static void removeChildren(OdfElement e) {
+        Node child;
+        while ((child = e.getFirstChild()) != null) 
+            e.removeChild(child);
     }
 
     public static void visitChildren(Node root) {
